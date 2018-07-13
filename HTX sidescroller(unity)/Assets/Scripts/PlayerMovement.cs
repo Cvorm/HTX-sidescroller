918530d434be3868fafb52f5954a1ef6c2aadb71
Unknown <cvorm16@student.aau.dk>
2018-07-13 02:05:04 +0200

    Added player movement

---
+++ HTX sidescroller(unity)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	public float playerMaxSpeed;
+	Rigidbody2D playerRB;
+	Animator animator;
+	bool isFacingRight;
+
+	void Start () {
+		// Gets the players rigidbody component aswell as the animator.
+		playerRB = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		float move = Input.GetAxis("Horizontal");
+		playerRB.velocity = new Vector2(move * playerMaxSpeed, playerRB.velocity.y);
+
+		if(move > 0 && !isFacingRight)
+		{
+			flip();
+		}
+		else if (move < 0 && isFacingRight)
+		{
+			flip();
+		}
+	}
+
+	void flip()
+	{
+		isFacingRight = !isFacingRight;
+		Vector3 scale = transform.localScale;
+		scale.x = scale.x * -1;
+		transform.localScale = scale;
+	}
+}
